e5a0ad9c70ce43e66af1bde10b295456c7b52d08
sh79ch <sh79ch@gmail.com>
2022-07-22 20:24:12 +0300

    online_courses

---
+++ online_courses.py
@@ -0,0 +1,217 @@
+class Student:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        self.average_rating = float()
+
+    def __str__(self):
+        grades_count = sum(len(self.grades[k]) for k in self.grades)
+        courses_in_progress_string = ', '.join(self.courses_in_progress)
+        finished_courses_string = ', '.join(self.finished_courses)
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) \
+            / grades_count
+        return f'Имя: {self.name}\n' \
+            f'Фамилия: {self.surname}\n' \
+            f'Средняя оценка за домашнее задание: {self.average_rating}\n' \
+            f'Курсы в процессе обучения: {courses_in_progress_string}\n' \
+            f'Завершенные курсы: {finished_courses_string}'
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in \
+                self.courses_in_progress and course \
+                in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Сравнение некорректно')
+            return
+        return self.average_rating < other.average_rating
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.average_rating = float()
+        self.grades = {}
+
+    def __str__(self):
+        grades_count = sum(len(self.grades[k]) for k in self.grades)
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) / \
+            grades_count
+        return f'Имя: {self.name}\nФамилия: {self.surname}\
+            \nСредняя оценка за лекции: {self.average_rating}'
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Cравнение некорректно')
+            return
+        return self.average_rating < other.average_rating
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached \
+                and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+
+best_lecturer_1 = Lecturer('Олег', ' Булыгин')
+best_lecturer_1.courses_attached += ['Python']
+best_lecturer_2 = Lecturer('Анатолий', 'Корсаков')
+best_lecturer_2.courses_attached += ['Java']
+best_lecturer_3 = Lecturer('Евгений', 'Шмаргунов')
+best_lecturer_3.courses_attached += ['Python']
+
+cool_reviewer_1 = Reviewer('Александр', 'Бардин')
+cool_reviewer_1.courses_attached += ['Python']
+cool_reviewer_1.courses_attached += ['Java']
+cool_reviewer_2 = Reviewer('Алена', 'Хохлова')
+cool_reviewer_2.courses_attached += ['Python']
+cool_reviewer_2.courses_attached += ['Java']
+
+student_1 = Student('Нина', 'Свиридова')
+student_1.courses_in_progress += ['Python']
+student_1.finished_courses += ['Введение в программирование']
+student_2 = Student('Владимир', 'Попов')
+student_2.courses_in_progress += ['Java']
+student_2.finished_courses += ['Введение в программирование']
+student_3 = Student('Петр', 'Киринов')
+student_3.courses_in_progress += ['Python']
+student_3.finished_courses += ['Введение в программирование']
+
+student_1.rate_hw(best_lecturer_1, 'Python', 10)
+student_1.rate_hw(best_lecturer_1, 'Python', 10)
+student_1.rate_hw(best_lecturer_1, 'Python', 10)
+student_1.rate_hw(best_lecturer_2, 'Python', 5)
+student_1.rate_hw(best_lecturer_2, 'Python', 7)
+student_1.rate_hw(best_lecturer_2, 'Python', 8)
+student_1.rate_hw(best_lecturer_1, 'Python', 7)
+student_1.rate_hw(best_lecturer_1, 'Python', 8)
+student_1.rate_hw(best_lecturer_1, 'Python', 9)
+student_2.rate_hw(best_lecturer_2, 'Java', 10)
+student_2.rate_hw(best_lecturer_2, 'Java', 8)
+student_2.rate_hw(best_lecturer_2, 'Java', 9)
+student_3.rate_hw(best_lecturer_3, 'Python', 5)
+student_3.rate_hw(best_lecturer_3, 'Python', 6)
+student_3.rate_hw(best_lecturer_3, 'Python', 7)
+
+cool_reviewer_1.rate_hw(student_1, 'Python', 8)
+cool_reviewer_1.rate_hw(student_1, 'Python', 9)
+cool_reviewer_1.rate_hw(student_1, 'Python', 10)
+cool_reviewer_2.rate_hw(student_2, 'Java', 8)
+cool_reviewer_2.rate_hw(student_2, 'Java', 7)
+cool_reviewer_2.rate_hw(student_2, 'Java', 9)
+cool_reviewer_2.rate_hw(student_3, 'Python', 8)
+cool_reviewer_2.rate_hw(student_3, 'Python', 7)
+cool_reviewer_2.rate_hw(student_3, 'Python', 9)
+cool_reviewer_2.rate_hw(student_3, 'Python', 8)
+cool_reviewer_2.rate_hw(student_3, 'Python', 7)
+cool_reviewer_2.rate_hw(student_3, 'Python', 9)
+
+print('Перечень студентов: ')
+print('-' * len('Перечень студентов:'))
+print(f'{student_1}\n\n{student_2}\n\n{student_3}')
+print()
+print()
+
+print('Перечень лекторов: ')
+print('-' * len('Перечень лекторов:'))
+print(f'{best_lecturer_1}\
+    \n\n{best_lecturer_2}\n\n{best_lecturer_3}')
+print()
+print()
+
+print('Перечень проверяющих:')
+print('-' * len('Перечень проверяющих:'))
+print(f'{cool_reviewer_1}\n\n{cool_reviewer_2}')
+print()
+print()
+
+print('Результат сравнения студентов (по средним оценкам за ДЗ):')
+print('-' * len('Результат сравнения студентов (по средним оценкам за ДЗ):'))
+
+s1 = student_1
+s2 = student_2
+s3 = student_3
+
+print('\n'.join(f'{i}' for i in sorted([s1, s2, s3], reverse=True)))
+
+print()
+print()
+
+print('Результат сравнения лекторов (по средним оценкам за лекции):')
+print('-' * len('Результат сравнения лекторов (по средним оценкам за лекции):'))
+
+l1 = best_lecturer_1
+l2 = best_lecturer_2
+l3 = best_lecturer_3
+
+print('\n'.join(f'{j}' for j in sorted([l1, l2, l3], reverse=True)))
+print()
+print()
+
+student_list = [student_1, student_2, student_3]
+lecturer_list = [best_lecturer_1, best_lecturer_2, best_lecturer_3]
+
+
+def student_rating(student_list, course_name):
+    sum_all = 0
+    count_all = 0
+    for stud in student_list:
+        if stud.courses_in_progress == [course_name]:
+            sum_all += stud.average_rating
+            count_all += 1
+    return sum_all / count_all
+
+
+def lecturer_rating(lecturer_list, course_name):
+    sum_all = 0
+    count_all = 0
+    for lect in lecturer_list:
+        if lect.courses_attached == [course_name]:
+            sum_all += lect.average_rating
+            count_all += 1
+    return sum_all / count_all
+
+
+print(f"Средняя оценка для всех студентов по курсу {'Python'}: \
+{student_rating(student_list, 'Python')}")
+print()
+
+print(f"Средняя оценка для всех лекторов по курсу \
+{'Python'}: {lecturer_rating(lecturer_list, 'Python')}")
+print()
+print()
+print('*' * len('Для выхода нажмите ENTER'))
+
+
+input('Для выхода нажмите ENTER')
+quit()
